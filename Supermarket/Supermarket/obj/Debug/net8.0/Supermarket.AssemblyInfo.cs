58ecdd28576ad7095d8672592b1dfab0763491e1
DRobbie39 <phangiadat123@gmail.com>
2024-03-12 21:25:25 +0700

    add section

--- Supermarket/Supermarket/obj/Debug/net8.0/Supermarket.AssemblyInfo.cs
+++ Supermarket/Supermarket/obj/Debug/net8.0/Supermarket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eeeae701fd56c59a1ae4a32d8948292504606c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+935aa32bc7ed37f94fc1249863ac48da3d6df493")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
